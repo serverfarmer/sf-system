ee4f524b7d9e159053b43fa3af683b6f3e0724dc
Tomasz Klim <github@tomaszklim.pl>
2019-11-16 13:42:28 +0100

    tested on nanopi-neo2

--- detect-architecture.sh
+++ detect-architecture.sh
@@ -4,7 +4,7 @@ mach=`uname -m`
 
 if [ "$mach" = "x86_64" ]; then
 	echo "amd64"
-elif [ "$mach" = "aarch64" ]; then  # Amazon EC2 A1 instances
+elif [ "$mach" = "aarch64" ]; then  # Allwinner H5 (eg. NanoPi NEO2), Amazon EC2 A1 instances
 	echo "arm64"
 elif [ "$mach" = "armv7l" ]; then  # Raspberry Pi
 	echo "armhf"
